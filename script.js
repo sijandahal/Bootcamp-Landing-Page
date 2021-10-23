bd2261d54bc42b82a2e0998b2f7703bf12fd1ce3
Sijan Dahal <dahalsijan7@gmail.com>
2021-10-23 14:51:55 +0545

    ädded search and scroll to top

--- script.js
+++ script.js
@@ -14,12 +14,12 @@ var ul = document.querySelector('.mobile-submenu-item');
 //open close toggle
 var menu = document.querySelector('.burger');
 var overlay = document.querySelector('.overlay');
-menu.addEventListener('click', function() {
+menu.addEventListener('click', function () {
   menu.classList.toggle('open');
   overlay.classList.toggle('visible');
   document.body.classList.toggle('open-nav')
   document.querySelector('.mobile-menu--nav').classList.toggle('open');
-}) 
+})
 
 //accordion
 
@@ -27,14 +27,39 @@ const items = document.querySelectorAll(".accordion button");
 
 function toggleAccordion() {
   const itemToggle = this.getAttribute('aria-expanded');
-  
+
   for (i = 0; i < items.length; i++) {
     items[i].setAttribute('aria-expanded', 'false');
   }
-  
+
   if (itemToggle == 'false') {
     this.setAttribute('aria-expanded', 'true');
   }
 }
 
 items.forEach(item => item.addEventListener('click', toggleAccordion));
+
+//scrolltotopbtn
+
+
+var scrollToTopBtn = document.querySelector(".back-to-top ");
+var rootElement = document.documentElement;
+
+function handleScroll() {
+  var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
+  if (rootElement.scrollTop / scrollTotal > 0.1) {
+    scrollToTopBtn.classList.add("active");
+  } else {
+    scrollToTopBtn.classList.remove("active");
+  }
+}
+
+function scrollToTop() {
+  // Scroll to top logic
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
+scrollToTopBtn.addEventListener("click", scrollToTop);
+document.addEventListener("scroll", handleScroll);
